a2f9fef714074f8b741f969fab21a6483307e60c
Luiz Fernando <lfrprazeres@gmail.com>
2022-11-13 20:04:55 -0300

    4.2 - função criarTarefa

--- src/store/middlewares/categorias.js
+++ src/store/middlewares/categorias.js
@@ -2,6 +2,7 @@ import { createStandaloneToast } from '@chakra-ui/toast';
 import { createListenerMiddleware } from '@reduxjs/toolkit';
 import categoriasService from 'services/categorias';
 import { adicionarTodasAsCategorias, carregarCategorias, carregarUmaCategoria } from 'store/reducers/categorias';
+import criarTarefa from './utils/criarTarefa';
 
 export const listener = createListenerMiddleware();
 const { toast } = createStandaloneToast();
@@ -9,41 +10,16 @@ const { toast } = createStandaloneToast();
 listener.startListening({
   actionCreator: carregarCategorias,
   effect: async (action, { dispatch, fork, unsubscribe }) => {
-    toast({
-      title: 'Carregando',
-      description: 'Carregando categorias',
-      status: 'loading',
-      duration: 2000,
-      isClosable: true
+    await criarTarefa({
+      fork,
+      dispatch,
+      action: adicionarTodasAsCategorias,
+      busca: categoriasService.buscar,
+      textoCarregando: 'Carregando categorias',
+      textoSucesso: 'Categorias carregadas com sucesso!',
+      textoErro: 'Erro na busca de categorias',
     });
-    const tarefa = fork(async api => {
-      await api.delay(1000);
-      return await categoriasService.buscar();
-    });
-
-    const resposta = await tarefa.result;
-
-    if (resposta.status === 'ok') {
-      toast({
-        title: 'Sucesso!',
-        description: 'Categorias carregadas com sucesso!',
-        status: 'success',
-        duration: 2000,
-        isClosable: true
-      });
-      dispatch(adicionarTodasAsCategorias(resposta.value));
-      unsubscribe();
-    }
-
-    if (resposta.status === 'rejected') {
-      toast({
-        title: 'Erro',
-        description: 'Erro na busca de categorias',
-        status: 'error',
-        duration: 2000,
-        isClosable: true
-      });
-    }
+    unsubscribe();
   }
 });
 
---
+++ src/store/middlewares/utils/criarTarefa.js
@@ -0,0 +1,48 @@
+import { createStandaloneToast } from '@chakra-ui/toast';
+
+const { toast } = createStandaloneToast();
+
+export default async function criarTarefa({
+  fork,
+  dispatch,
+  busca,
+  action,
+  textoCarregando,
+  textoSucesso,
+  textoErro
+}) {
+  toast({
+    title: 'Carregando',
+    description: textoCarregando,
+    status: 'loading',
+    duration: 2000,
+    isClosable: true
+  });
+  const tarefa = fork(async api => {
+    await api.delay(1000);
+    return await busca();
+  });
+
+  const resposta = await tarefa.result;
+
+  if (resposta.status === 'ok') {
+    toast({
+      title: 'Sucesso!',
+      description: textoSucesso,
+      status: 'success',
+      duration: 2000,
+      isClosable: true
+    });
+    dispatch(action(resposta.value));
+  }
+
+  if (resposta.status === 'rejected') {
+    toast({
+      title: 'Erro',
+      description: textoErro,
+      status: 'error',
+      duration: 2000,
+      isClosable: true
+    });
+  }
+}
